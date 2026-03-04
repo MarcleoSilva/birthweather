eefb0c289839c431fa507b8e2a5a85fef0c532d7
Marcelo Araújo Silva <marceloaraujosilva739@gmail.com>
2026-03-04 19:07:25 -0300

    simple wiring. now, it really works to get the city and country and return the lat and lon

--- src/main/java/com/projects/birthweather/services/BirthWeatherService.java
+++ src/main/java/com/projects/birthweather/services/BirthWeatherService.java
@@ -9,9 +9,14 @@ import org.springframework.stereotype.Service;
 public class BirthWeatherService {
 
     private final BirthDataRepository repository;
+    private final GeoDataService geoDataService;
 
-    public BirthWeatherService(BirthDataRepository repository) {
+    public BirthWeatherService(
+            BirthDataRepository repository,
+            GeoDataService geoDataService
+            ) {
         this.repository = repository;
+        this.geoDataService = geoDataService;
     }
 
     public BirthData registerBirthData(BirthDataRequestDTO data){
@@ -20,6 +25,10 @@ public class BirthWeatherService {
         bd.setCity(data.city());
         bd.setDate(data.date());
         bd.setCountry(data.country());
+
+        bd.setLatitude(geoDataService.getGeolocation(data.city(), data.country()).getLatitude());
+
+        bd.setLongitude(geoDataService.getGeolocation(data.city(), data.country()).getLongitude());
 
         return repository.save(bd);
     }
