78161d9388bbc27e37693c8652ccb9f608f1ddee
Marcelo Araújo Silva <marceloaraujosilva739@gmail.com>
2026-03-10 15:43:50 -0300

    Finished back-end part: - stores data - analyze data - returns the fields

--- src/main/java/com/projects/birthweather/controller/BirthWeatherController.java
+++ src/main/java/com/projects/birthweather/controller/BirthWeatherController.java
@@ -2,7 +2,9 @@ package com.projects.birthweather.controller;
 
 import com.projects.birthweather.domain.birthdata.BirthData;
 import com.projects.birthweather.domain.birthdata.BirthDataRequestDTO;
+import com.projects.birthweather.domain.stats.WeatherStatsDTO;
 import com.projects.birthweather.services.BirthWeatherService;
+import com.projects.birthweather.services.WeatherStatsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,11 +16,15 @@ public class BirthWeatherController {
 
     @Autowired
     private BirthWeatherService birthWeatherService;
+    @Autowired
+    private WeatherStatsService weatherStatsService;
+
 
     @PostMapping
-    public ResponseEntity<BirthData> create(@RequestBody BirthDataRequestDTO body){
-        BirthData newBirthdata = this.birthWeatherService.registerBirthData(body);
-        return ResponseEntity.ok(newBirthdata);
+    public ResponseEntity<WeatherStatsDTO> create(@RequestBody BirthDataRequestDTO body){
+        BirthData newBirthData = this.birthWeatherService.registerBirthData(body);
+
+        return ResponseEntity.ok(this.weatherStatsService.analyzeData(newBirthData));
     }
 
 
